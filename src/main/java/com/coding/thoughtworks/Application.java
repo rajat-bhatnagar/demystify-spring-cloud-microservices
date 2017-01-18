8a2e18f70d6cfa0f822efcbcae0f29ed2837ad0a
Rajat Bhatnagar <rajat_bhatnagar@outlook.com>
2017-01-17 23:45:24 -0500

    Initial Commit

---
+++ src/main/java/com/coding/thoughtworks/Application.java
@@ -0,0 +1,20 @@
+package com.coding.thoughtworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@RequestMapping(value="/greeting")
+	public String sayHello(){
+		return "Hello from Spring Boot!";
+	}
+}
